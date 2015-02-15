a705768d57007eee9ead5247897a7014856b4ab0
zparnold <zparnold@wpi.edu>
2015-02-15 18:59:25 -0500

    Adds constraint holder to package

--- CS4341 - Project 4/src/main/CSPSolver.java
+++ CS4341 - Project 4/src/main/CSPSolver.java
@@ -8,5 +8,28 @@ package main;
  *
  */
 public class CSPSolver {
+	
+	private ConstraintHolder holder;
+	private int counter = 0;
+
+	public CSPSolver(ConstraintHolder holder){
+		this.holder = holder;
+	}
+	
+	/**
+	 * Searches using the backtracking search algorithm for a solution
+	 * to the CSP (Used algorithm from Russel and Norvig)
+	 * @return boolean, whether or not the constraints are attainable
+	 */
+	public boolean backtrackSearch(){
+		//Update statistics
+		counter++;
+		
+		Object var = holder.getNextUnsatVar();
+		
+		return holder.getFlag();
+		
+		
+	}
 
 }
---
+++ CS4341 - Project 4/src/main/ConstraintHolder.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package main;
+
+/**
+ * @author Zach
+ *
+ */
+public class ConstraintHolder {
+	//Whether or not the problem is attainable
+	private boolean flag = false;
+	
+
+	public boolean getFlag() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	public Object getNextUnsatVar() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
