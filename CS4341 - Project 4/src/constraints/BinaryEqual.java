4494c255c28ae46657e67cbc3ff4680da94cc3d9
zparnold <zparnold@wpi.edu>
2015-02-16 13:07:46 -0500

    Adds isValid methods to Binary constraint classes

--- CS4341 - Project 4/src/constraints/BinaryEqual.java
+++ CS4341 - Project 4/src/constraints/BinaryEqual.java
@@ -1,9 +1,28 @@
 package constraints;
 
+import java.util.ArrayList;
+
 import main.Item;
 import main.ItemBag;
 
 public class BinaryEqual implements Constraint {
+
+	/**
+	 * 
+	 * @param bag
+	 * @param item1
+	 * @param item2
+	 * @return
+	 */
+	//This method is in here because the overridden method only works for
+	//unary constraints...maybe we could switch the interface to Array
+	//List of type Item and just check the first one for unary constraints?
+	public boolean isValid(ItemBag bag, Item item1, Item item2) {
+		ArrayList<Item> check = new ArrayList<Item>();
+		check.add(item1);
+		check.add(item2);
+		return (bag.getItems().containsAll(check));
+	}
 
 	@Override
 	public boolean isValid(ItemBag bag, Item item) {
